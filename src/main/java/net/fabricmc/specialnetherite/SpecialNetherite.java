bf0dc3f665ebfb91534b6d306a670db9f1a56d3f
JWGardiner <86971433+JWGardiner@users.noreply.github.com>
2021-10-07 09:11:37 +0100

    V0.4

--- src/main/java/net/fabricmc/specialnetherite/SpecialNetherite.java
+++ src/main/java/net/fabricmc/specialnetherite/SpecialNetherite.java
@@ -12,39 +12,29 @@ import net.fabricmc.fabric.api.tool.attribute.v1.FabricToolTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.Material;
-import net.minecraft.block.sapling.OakSaplingGenerator;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.item.*;
+import net.minecraft.network.MessageType;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.sound.SoundEvent;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.structure.rule.BlockMatchRuleTest;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.intprovider.ConstantIntProvider;
 import net.minecraft.util.registry.BuiltinRegistries;
 import net.minecraft.util.registry.Registry;
 import net.minecraft.util.registry.RegistryKey;
-import net.minecraft.world.Heightmap;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.biome.BiomeEffects;
 import net.minecraft.world.biome.GenerationSettings;
 import net.minecraft.world.biome.SpawnSettings;
 import net.minecraft.world.gen.GenerationStep;
 import net.minecraft.world.gen.YOffset;
-import net.minecraft.world.gen.decorator.Decorator;
-import net.minecraft.world.gen.decorator.HeightmapDecoratorConfig;
 import net.minecraft.world.gen.decorator.RangeDecoratorConfig;
 import net.minecraft.world.gen.feature.*;
-import net.minecraft.world.gen.feature.size.TwoLayersFeatureSize;
-import net.minecraft.world.gen.foliage.BlobFoliagePlacer;
 import net.minecraft.world.gen.heightprovider.UniformHeightProvider;
-import net.minecraft.world.gen.stateprovider.SimpleBlockStateProvider;
 import net.minecraft.world.gen.surfacebuilder.ConfiguredSurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilder.SurfaceBuilder;
 import net.minecraft.world.gen.surfacebuilder.TernarySurfaceConfig;
-import net.minecraft.world.gen.trunk.StraightTrunkPlacer;
-
-import static net.fabricmc.loom.configuration.MavenPublication.configure;
 
 
 public class SpecialNetherite implements ModInitializer {
@@ -106,7 +96,7 @@ public class SpecialNetherite implements ModInitializer {
 
 		@Override
 		public float getAttackDamage() {
-			return 14;
+			return 0;
 		}
 
 		@Override
@@ -189,11 +179,11 @@ public class SpecialNetherite implements ModInitializer {
 			return 0;
 		}
 	}
-	public static ToolItem OVER_NETHERITE_SWORD = new SwordItem(OverworldNetherite.INSTANCE, -7, -3.4f, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem OVER_NETHERITE_PICKAXE = new OverworldNetherite.OverPickaxe(OverworldNetherite.INSTANCE, -11, -2.8f, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem OVER_NETHERITE_AXE = new OverworldNetherite.OverAxe(OverworldNetherite.INSTANCE, -7, -2, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem OVER_NETHERITE_SHOVEL = new ShovelItem(OverworldNetherite.INSTANCE, -7.5f, 0, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem OVER_NETHERITE_HOE = new OverworldNetherite.OverHoe(OverworldNetherite.INSTANCE, -13, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem OVER_NETHERITE_SWORD = new SwordItem(OverworldNetherite.INSTANCE, 20, -3.4f, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem OVER_NETHERITE_PICKAXE = new OverworldNetherite.OverPickaxe(OverworldNetherite.INSTANCE, 10, -2.8f, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem OVER_NETHERITE_AXE = new OverworldNetherite.OverAxe(OverworldNetherite.INSTANCE, 32, -2, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem OVER_NETHERITE_SHOVEL = new ShovelItem(OverworldNetherite.INSTANCE, 10f, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem OVER_NETHERITE_HOE = new OverworldNetherite.OverHoe(OverworldNetherite.INSTANCE, 1, 0, new Item.Settings().group(TOOLS).fireproof());
 	public static final ArmorMaterial OVERWORLD_ARMOR_MATERIAL = new OverworldArmorMaterial();
 	public static final Item OVERWORLD_NETHERITE_HELMET = new ArmorItem(OVERWORLD_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(TOOLS).fireproof());
 	public static final Item OVERWORLD_NETHERITE_CHESTPLATE = new ArmorItem(OVERWORLD_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(TOOLS).fireproof());
@@ -220,7 +210,7 @@ public class SpecialNetherite implements ModInitializer {
 
 		@Override
 		public float getAttackDamage() {
-			return 13;
+			return 0;
 		}
 
 		@Override
@@ -290,7 +280,7 @@ public class SpecialNetherite implements ModInitializer {
 		@Override
 		public String getName() {
 			// Must be all lowercase
-			return "overworld";
+			return "nether";
 		}
 
 		@Override
@@ -303,11 +293,11 @@ public class SpecialNetherite implements ModInitializer {
 			return 0;
 		}
 	}
-	public static ToolItem NETHER_NETHERITE_SWORD = new SwordItem(NetherNetherite.INSTANCE, -7, -3.4f, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem NETHER_NETHERITE_PICKAXE = new NetherNetherite.NetherPickaxe(NetherNetherite.INSTANCE, -11, -2.8f, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem NETHER_NETHERITE_AXE = new NetherNetherite.NetherAxe(NetherNetherite.INSTANCE, -7, -2, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem NETHER_NETHERITE_SHOVEL = new ShovelItem(NetherNetherite.INSTANCE, -7.5f, 0, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem NETHER_NETHERITE_HOE = new NetherNetherite.NetherHoe(NetherNetherite.INSTANCE, -13, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem NETHER_NETHERITE_SWORD = new SwordItem(NetherNetherite.INSTANCE, 20, -3.4f, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem NETHER_NETHERITE_PICKAXE = new NetherNetherite.NetherPickaxe(NetherNetherite.INSTANCE, 10, -2.8f, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem NETHER_NETHERITE_AXE = new NetherNetherite.NetherAxe(NetherNetherite.INSTANCE, 32, -2, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem NETHER_NETHERITE_SHOVEL = new ShovelItem(NetherNetherite.INSTANCE, 10f, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem NETHER_NETHERITE_HOE = new NetherNetherite.NetherHoe(NetherNetherite.INSTANCE, 1, 0, new Item.Settings().group(TOOLS).fireproof());
 	public static final ArmorMaterial NETHER_ARMOR_MATERIAL = new NetherArmorMaterial();
 	public static final Item NETHER_NETHERITE_HELMET = new ArmorItem(NETHER_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(TOOLS).fireproof());
 	public static final Item NETHER_NETHERITE_CHESTPLATE = new ArmorItem(NETHER_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(TOOLS).fireproof());
@@ -334,7 +324,7 @@ public class SpecialNetherite implements ModInitializer {
 
 		@Override
 		public float getAttackDamage() {
-			return 13;
+			return 0;
 		}
 
 		@Override
@@ -371,7 +361,7 @@ public class SpecialNetherite implements ModInitializer {
 		}
 
 	}
-	public class EndArmorMaterial implements ArmorMaterial {
+	public static class EndArmorMaterial implements ArmorMaterial {
 		private static final int[] BASE_DURABILITY = new int[] {15, 17, 18, 13};
 		private static final int[] PROTECTION_VALUES = new int[] {4, 7, 9, 3};
 
@@ -403,7 +393,7 @@ public class SpecialNetherite implements ModInitializer {
 		@Override
 		public String getName() {
 			// Must be all lowercase
-			return "overworld";
+			return "end";
 		}
 
 		@Override
@@ -416,12 +406,12 @@ public class SpecialNetherite implements ModInitializer {
 			return 0;
 		}
 	}
-	public static ToolItem END_NETHERITE_SWORD = new SwordItem(EndNetherite.INSTANCE, -7, -3.4f, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem END_NETHERITE_PICKAXE = new EndNetherite.EndPickaxe(EndNetherite.INSTANCE, -11, -2.8f, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem END_NETHERITE_AXE = new EndNetherite.EndAxe(EndNetherite.INSTANCE, -7, -2, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem END_NETHERITE_SHOVEL = new ShovelItem(EndNetherite.INSTANCE, -7.5f, 0, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem END_NETHERITE_HOE = new EndNetherite.EndHoe(EndNetherite.INSTANCE, -13, 0, new Item.Settings().group(TOOLS).fireproof());
-	public static final ArmorMaterial END_ARMOR_MATERIAL = new OverworldArmorMaterial();
+	public static ToolItem END_NETHERITE_SWORD = new SwordItem(EndNetherite.INSTANCE, 20, -3.4f, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem END_NETHERITE_PICKAXE = new EndNetherite.EndPickaxe(EndNetherite.INSTANCE, 10, -2.8f, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem END_NETHERITE_AXE = new EndNetherite.EndAxe(EndNetherite.INSTANCE, 32, -2, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem END_NETHERITE_SHOVEL = new ShovelItem(EndNetherite.INSTANCE, 10f, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem END_NETHERITE_HOE = new EndNetherite.EndHoe(EndNetherite.INSTANCE, 1, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static final ArmorMaterial END_ARMOR_MATERIAL = new EndArmorMaterial();
 	public static final Item END_NETHERITE_HELMET = new ArmorItem(END_ARMOR_MATERIAL, EquipmentSlot.HEAD, new Item.Settings().group(TOOLS).fireproof());
 	public static final Item END_NETHERITE_CHESTPLATE = new ArmorItem(END_ARMOR_MATERIAL, EquipmentSlot.CHEST, new Item.Settings().group(TOOLS).fireproof());
 	public static final Item END_NETHERITE_LEGGINGS = new ArmorItem(END_ARMOR_MATERIAL, EquipmentSlot.LEGS, new Item.Settings().group(TOOLS).fireproof());
@@ -484,7 +474,7 @@ public class SpecialNetherite implements ModInitializer {
 
 		@Override
 		public float getAttackDamage() {
-			return 16;
+			return 13;
 		}
 
 		@Override
@@ -572,7 +562,7 @@ public class SpecialNetherite implements ModInitializer {
 	public static ToolItem STRONG_NETHERITE_PICKAXE = new StrongNetherite.StrongPickaxe(StrongNetherite.INSTANCE, -11, -2.8f, new Item.Settings().group(TOOLS).fireproof());
 	public static ToolItem STRONG_NETHERITE_AXE = new StrongNetherite.StrongAxe(StrongNetherite.INSTANCE, -7, -2, new Item.Settings().group(TOOLS).fireproof());
 	public static ToolItem STRONG_NETHERITE_SHOVEL = new ShovelItem(StrongNetherite.INSTANCE, -7.5f, 0, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem STRONG_NETHERITE_HOE = new StrongNetherite.StrongHoe(StrongNetherite.INSTANCE, -16, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem STRONG_NETHERITE_HOE = new StrongNetherite.StrongHoe(StrongNetherite.INSTANCE, -13, 0, new Item.Settings().group(TOOLS).fireproof());
 
 	//Enchanted Tools
 	static class EnchantedNetherite implements ToolMaterial {
@@ -590,7 +580,7 @@ public class SpecialNetherite implements ModInitializer {
 
 		@Override
 		public float getAttackDamage() {
-			return 8;
+			return 11;
 		}
 
 		@Override
@@ -632,7 +622,7 @@ public class SpecialNetherite implements ModInitializer {
 	public static ToolItem ENCHANTED_NETHERITE_PICKAXE = new EnchantedNetherite.EnchantedPickaxe(EnchantedNetherite.INSTANCE, -11, -2.8f, new Item.Settings().group(TOOLS).fireproof());
 	public static ToolItem ENCHANTED_NETHERITE_AXE = new EnchantedNetherite.EnchantedAxe(EnchantedNetherite.INSTANCE, -7, -3, new Item.Settings().group(TOOLS).fireproof());
 	public static ToolItem ENCHANTED_NETHERITE_SHOVEL = new ShovelItem(EnchantedNetherite.INSTANCE, -10.5f, -3, new Item.Settings().group(TOOLS).fireproof());
-	public static ToolItem ENCHANTED_NETHERITE_HOE = new EnchantedNetherite.EnchantedHoe(EnchantedNetherite.INSTANCE, -16, 0, new Item.Settings().group(TOOLS).fireproof());
+	public static ToolItem ENCHANTED_NETHERITE_HOE = new EnchantedNetherite.EnchantedHoe(EnchantedNetherite.INSTANCE, -13, 0, new Item.Settings().group(TOOLS).fireproof());
 
 
 	private static ConfiguredFeature<?, ?> ENCHANTED_CRYSTAL_OVERWORLD = Feature.ORE
